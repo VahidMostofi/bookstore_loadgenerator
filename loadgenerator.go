095a64d2f016f9c43c9b944db2b4398913d8e961
Vahid Mostofi <vahidmostofi@outlook.com>
2020-03-03 19:03:34 -0700

    fixed bug in concurrency infor counting

--- loadgenerator.go
+++ loadgenerator.go
@@ -181,7 +181,9 @@ func (t *TestResult) computeConcurrencyInfo(starts, ends []int64, firstRequestTi
 		end := int((ends[i] - firstRequestTime) / unitConvertor)
 		// fmt.Println(start, end)
 		for j := start; j < end; j++ {
-			units[j]++
+			if j < len(units){
+				units[j]++
+			}
 		}
 	}
 
